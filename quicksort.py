4f202460cab17e401fb2831663617358424d5ecf
Enes Battal <52421169+m3n3s@users.noreply.github.com>
2021-12-23 17:05:34 +0300

    delete spaces

--- quicksort.py
+++ quicksort.py
@@ -52,7 +52,7 @@ def quick_sort(start, end, array):
         quick_sort(p + 1, end, array)
           
 # Driver code
-myArray = [ 10, 7, 8, 9, 1, 5 ]
+myArray = [10, 7, 8, 9, 1, 5]
 quick_sort(0, len(myArray) - 1, myArray)
   
 print(f'Sorted array: {myArray}')
